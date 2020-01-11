c0e66499a0a2416672b7d8e077440a3a4945df2e
Kgodthecreator <57558218+Kgodthecreator@users.noreply.github.com>
2020-01-11 10:44:02 +0530

    Add files via upload

---
+++ Slingshot.js
@@ -0,0 +1,25 @@
+class SlingShot{
+    constructor(bodyA,pointB){
+        var options={
+            bodyA:bodyA,
+            pointB:pointB,
+            stiffness:6,
+            length:6    
+            }
+            this.pointB=pointB;
+            this.sling = Constraint.create(options);
+            World.add(world, this.sling);
+    }
+    fly(){
+        this.sling.bodyA = null;
+    }
+    display(){
+        if(this.sling.bodyA){
+        var pointA = this.sling.bodyA.position;
+        var pointB = this.pointB;
+        strokeWeight(4);
+        stroke("blue");
+        line(pointA.x,pointA.y,pointB.x,pointB.y);
+    }
+}
+}
